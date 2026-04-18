a34fc8b52cf04a8dfd40b5f7d4a3f3f451e736cb
asthabhardwaj09 <asthabhardwaj529@gmail.com>
2026-04-18 23:58:14 +0530

    initialization

---
+++ Interview/Question_JS/day1.js
@@ -0,0 +1,30 @@
+// // re decleration
+
+// var a =34
+// var a =20
+// // var a =34
+
+// // console.log(a)
+
+// // let a=20
+// let a=50
+// // console.log(a)
+
+// if(true){
+//     let a=50
+//     // let a=40
+//     // console.log(a)
+// }
+
+const val1= 20
+// const val1=50
+console.log(val1)
+
+if (true) {
+    const val3=67;
+    // const val3=67;
+    console.log(val3)
+    
+} else {
+    console.log("nothing")
+}
