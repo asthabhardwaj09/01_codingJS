51989e109ffd818d11afa4d417d817bc80dfbb6a
asthabhardwaj09 <asthabhardwaj529@gmail.com>
2026-04-20 23:56:08 +0530

    hoisting

---
+++ Interview/day2.js
@@ -0,0 +1,11 @@
+greet(); // Output: "Hello World" (Works because of hoisting)
+
+function greet() {
+  console.log("Hello World");
+}
+
+
+
+console.log(user); // Output: undefined
+var user = "Alice";
+console.log(user); // Output: "Alice"
